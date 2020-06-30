c2a167e455d997f3737217439364f5a8f93e77bb
jaywcjlove <jaywcjlove@users.noreply.github.com>
2020-06-30 09:32:28 +0000

    deploy: 3e6fc3149c203fa371b1a37898d09ddb487ca585

---
+++ precache-manifest.5d0c495bbaa7b2c044ef56ac3ad381a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "644ea846a59cf1c1d1c259003bb019df",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b7fd52d401589948739a",
+    "url": "./static/css/main.73878221.chunk.css"
+  },
+  {
+    "revision": "5cc6fe78832b159fedbe",
+    "url": "./static/js/2.5d8f6bab.chunk.js"
+  },
+  {
+    "revision": "b7fd52d401589948739a",
+    "url": "./static/js/main.3e19c1b9.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
